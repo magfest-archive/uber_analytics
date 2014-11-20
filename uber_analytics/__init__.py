e5242b39dc1598dec02a26e7f5d10b4fba2d0863
Dom <dom@magfest.org>
2014-11-20 01:08:52 -0500

    mount a static dir in this plugin for /magfest/static

--- uber_analytics/__init__.py
+++ uber_analytics/__init__.py
@@ -4,3 +4,12 @@ from uber_analytics import numbers, graphing
 
 config = parse_config(__file__)
 django.conf.settings.TEMPLATE_DIRS.insert(0, join(config['module_root'], 'templates'))
+
+analytics_app_config = {
+    '/static': {
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': os.path.join(config['module_root'], 'static')
+    }
+}
+
+cherrypy.tree.mount(Root(), PATH + '/analytics', analytics_app_config)
