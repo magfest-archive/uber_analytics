098a968fb06b52e36e39b001837c206a6b5212fd
Daniel Evans <migetman9@gmail.com>
2016-11-19 17:28:50 -0500

    begin zipcode

---
+++ uber_analytics/site_sections/map.py
@@ -0,0 +1,29 @@
+from uber.common import *
+from collections import Counter
+from uszipcode import ZipcodeSearchEngine
+
+@all_renderable(c.STATS)
+class Root:
+    zips_counter = Counter()
+    zips = {}
+    center = ZipcodeSearchEngine().by_zipcode("20745")
+
+    def index(self):
+        return {
+            'zip_counts': self.zips_counter,
+            'center': self.center,
+        }
+
+    def refresh(self, session):
+        zips = {}
+        self.zips_counter = Counter()
+        attendees = session.query(Attendee).all()
+        for person in attendees:
+            if person.zip_code:
+                self.zips_counter[person.zip_code] += 1
+
+        for z in self.zips_counter.keys():
+            zips[z] = ZipcodeSearchEngine().by_zipcode(z)
+
+        self.zips = zips
+        raise HTTPRedirect("index")
