9b246b77491c29a492fa72089bad274f983e2c30
jcooter <jcooter@oceanius.com>
2017-05-04 15:46:38 -0400

    Adding analytics API service

--- uber_analytics/__init__.py
+++ uber_analytics/__init__.py
@@ -1,6 +1,8 @@
 from uber.common import *
 from uber_analytics._version import __version__
 
+from uber_analytics import api
+
 config = parse_config(__file__)
 mount_site_sections(config['module_root'])
 static_overrides(join(config['module_root'], 'static'))
