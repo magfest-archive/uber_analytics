1707f6814e2724bf9003901e5f19ca0ad0fb99d4
Daniel Evans <migetman9@gmail.com>
2017-06-29 16:24:44 -0700

    add ability to get attendee info from map

--- uber_analytics/site_sections/map.py
+++ uber_analytics/site_sections/map.py
@@ -56,6 +56,40 @@ class Root:
                                       VincentyDistance((x["Latitude"], x["Longitude"]), center_coord).miles,
                                       "%.2f" % float(self.zips_counter[x['Zipcode']]/total_count * 100)])
 
+    @csv_file
+    def attendee_in_radius_csv(self, out, session, radius, **params):
+        res = ZipcodeSearchEngine().by_coordinate(self.center["Latitude"], self.center["Longitude"], radius=int(radius),
+                                                  returns=0)
+        cols = [getattr(Attendee, col.name) for col in Attendee.__table__.columns]
+        out.writerow([col.name for col in cols])
+        if len(res) > 0:
+            keys = [x.to_dict().get('Zipcode') for x in res]
+            filter = Attendee.badge_status.in_([c.NEW_STATUS, c.COMPLETED_STATUS])
+            attendees = session.query(Attendee).filter(Attendee.zip_code.in_(keys)).filter(filter, Attendee.can_spam).all()
+            for attendee in attendees:
+                row = []
+                for col in cols:
+                    if isinstance(col.type, Choice):
+                        # Choice columns are integers with a single value with an automatic
+                        # _label property, e.g. the "shirt" column has a "shirt_label"
+                        # property, so we'll use that.
+                        row.append(getattr(attendee, col.name + '_label'))
+                    elif isinstance(col.type, MultiChoice):
+                        # MultiChoice columns are comma-separated integer lists with an
+                        # automatic _labels property which is a list of string labels.
+                        # So we'll get that and then separate the labels with slashes.
+                        row.append(' / '.join(getattr(attendee, col.name + '_labels')))
+                    elif isinstance(col.type, UTCDateTime):
+                        # Use the empty string if this is null, otherwise use strftime.
+                        # Also you should fill in whatever actual format you want.
+                        val = getattr(attendee, col.name)
+                        row.append(val.strftime('%Y-%m-%d %H:%M:%S') if val else '')
+                    else:
+                        # For everything else we'll just dump the value, although we might
+                        # consider adding more special cases for things like foreign keys.
+                        row.append(getattr(attendee, col.name))
+                out.writerow(row)
+
     @ajax
     def set_center(self, session, **params):
         if params.get("zip"):
